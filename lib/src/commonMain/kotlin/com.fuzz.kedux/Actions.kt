2154033440cfead1720203a3c26bf20c7cde9709
Andrew Grosner <abgrosner@gmail.com>
2020-03-28 09:35:57 -0400

    [Actions] add tests for multiple actions. Add typed action with reducers.

--- lib/src/commonMain/kotlin/com.fuzz.kedux/Actions.kt
+++ lib/src/commonMain/kotlin/com.fuzz.kedux/Actions.kt
@@ -10,4 +10,13 @@ data class MultiAction(val actions: List<Any>)
 /**
  * Constructs an object that contains multiple, sequential objects. See [MultiAction]
  */
-fun multipleActionOf(vararg actions: Any) = MultiAction(*actions.toList())
+fun multipleActionOf(vararg actions: Any) = MultiAction(actions.toList())
+
+/**
+ * A class Action class, this is used to provide actions that are not purely class types. This is for class use, but
+ * preferably use data class objects.
+ */
+interface Action<T> {
+
+    val type: T
+}
--- lib/src/commonMain/kotlin/com.fuzz.kedux/Reducers.kt
+++ lib/src/commonMain/kotlin/com.fuzz.kedux/Reducers.kt
@@ -8,27 +8,39 @@ abstract class Reducer<S : Any> {
 }
 
 inline fun <reified S : Any> anyReducer(crossinline fn: (state: S, action: Any) -> S) =
-    object : Reducer<S>() {
-        override fun reduce(state: S, action: Any): S = fn(state, action)
-        override val stateClass: KClass<S>
-            get() = S::class
-    }
+        object : Reducer<S>() {
+            override fun reduce(state: S, action: Any): S = fn(state, action)
+            override val stateClass: KClass<S>
+                get() = S::class
+        }
 
 /**
  * Constructs a typesafe action that only accepts the type argument [A] as an action. Useful for sealed classes.
  */
 inline fun <reified S : Any, reified A> typedReducer(crossinline fn: (state: S, action: A) -> S) =
-    anyReducer<S> { state, action ->
-        when (action is A) {
-            true -> fn(state, action)
-            else -> state
+        anyReducer<S> { state, action ->
+            when (action is A) {
+                true -> fn(state, action)
+                else -> state
+            }
         }
-    }
+
+/**
+ * Constructs a typed action that expects a key type passed into the reducer to distinguish actions by their type.
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified S : Any, reified AT : Action<A>, A> actionTypeReducer(type: A, crossinline fn: (state: S, action: AT) -> S) =
+        anyReducer<S> { state, action ->
+            when (action is AT && action.type === type) {
+                true -> fn(state, action)
+                else -> state
+            }
+        }
 
 /**
  * Combines a set of reducers into smaller pieces.
  */
 inline fun <reified S : Any> combineReducers(vararg reducers: Reducer<S>): Reducer<S> =
-    anyReducer { state: S, action: Any ->
-        reducers.foldRight(state) { reducer, localState -> reducer.reduce(localState, action) }
-    }
+        anyReducer { state: S, action: Any ->
+            reducers.foldRight(state) { reducer, localState -> reducer.reduce(localState, action) }
+        }
